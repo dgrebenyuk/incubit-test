e710a425a9d0a2919383d71e7581ad71334944ad
Denis Grebenyuk <dngrebenyuk@gmail.com>
2019-05-14 02:40:28 +0300

    make user singular resource, improve sign up style

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
-  validates :password, confirmation: true, length: { minimum: 8 }
+  validates :password, confirmation: true, length: { minimum: 8 }, if: :will_save_change_to_password_digest?
   validates :username, length: { minimum: 5 }, on: :update
 
   before_create :set_username
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resource :user, except: :edit
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'signup', to: 'users#new', as: 'signup'
