fcd156b7806da454f0b3e1f8dd4eba4afeb261ad
Denis Grebenyuk <dngrebenyuk@gmail.com>
2019-05-14 00:41:00 +0300

    add create user feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :password, confirmation: true, length: { minimum: 8 }
+  validates :username, length: { minimum: 5 }, on: :update
+
+  before_create :set_username
+
+  private
+
+  def set_username
+    self.username = self.email.split('@').first
+  end
 end
